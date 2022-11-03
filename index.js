2a1204e6b8dc4bb6c3b0860f897bbb7d114db2b3
AdityaV <91488738+adityav-Archer@users.noreply.github.com>
2022-11-03 11:46:12 -0700

    Changes to the config_dev

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const { readFile, readdir } = require('fs');
+const { readFile, readdir, writeFile,appendFile } = require('fs');
 const util = require('util');
 const readFileAsync = util.promisify(readFile);
+const writeFileAsync = util.promisify(writeFile)
 const process = require('process');
 const main = async (env) => {
     let shellFile = await readFileAsync(`dev/config.json`, "utf8");
     console.log("the contents are:" , shellFile);
     shellFile = JSON.parse(shellFile);
+    let fileContents = `export let config = ${JSON.stringify(shellFile)}`;
     console.log(shellFile);
+    writeFileAsync("./config_dev/config_test_file.js", fileContents);
+
 }
 
-let environment = process.env.environment ? process.env.environment : "dev"
+let environment = process.env.environment ? process.env.environment : "dev";
+console.log(environment);
 main(environment);
