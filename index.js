971dd328c3fdf615d93ca915599d05618dec566d
AdityaV <91488738+adityav-Archer@users.noreply.github.com>
2022-11-04 10:54:55 -0700

    Stage2 Testchanges

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const { readFile, readdir, writeFile,appendFile, writeFileSync } = require('fs');
+const { readFile, readdir, writeFile, writeFileSync, mkdirSync, existsSync, readFileSync } = require('fs');
 const util = require('util');
 const readFileAsync = util.promisify(readFile);
 const writeFileAsync = util.promisify(writeFile)
+const readDirAsync = util.promisify(readdir);
 const process = require('process');
 const { BlobServiceClient } = require("@azure/storage-blob");
-const mime = require('mime-types');
 
 const webContainerClient = (
     BlobServiceClient
-    .fromConnectionString(process.env.CONNECTION_STRING)
+    .fromConnectionString(process.env.CONNECTION_STRING ?? "DefaultEndpointsProtocol=https;AccountName=archcontextintelstorage;AccountKey=vCqcOwkNzsJgr0+SYUch6vOaBsVAOEkh1HJic+mf+7YErBRTuVtVO+3a8HsoPI8P36evrdVZzuJj2lJe8Eus2Q==;EndpointSuffix=core.windows.net")
     .getContainerClient('aditya-test')
 );
 const url = webContainerClient.url;
@@ -21,26 +21,43 @@ function upload(localPath, blobPath) {
     const blobClient = webContainerClient.getBlockBlobClient(blobPath);
     return blobClient.uploadFile(localPath, {
         blobHTTPHeaders: {
-            blobContentType: guessMimeType("config_test_file2.js"),
+            blobContentType: 'application/octet-stream',
             blobCacheControl: 'no-cache', // Disable caching since we want changes to be immediately visible in WebXR.
         }
     });
 }
 
-function guessMimeType(filename) {
-    return mime.lookup(filename) || 'application/octet-stream';
+
+
+async function getApplicationsList(env){
+    let dirPath = `${env}/apps`;
+    let filesList = await readDirAsync(dirPath);
+    let appsList = [];
+    console.log(filesList);
+    if(filesList.length === 0) return [];
+    filesList.forEach(async item => {
+        let app =  readFileSync(`${dirPath}/${item}`, "utf8");
+    
+        app = JSON.parse(app);
+        appsList.push(app);
+    })
+    return appsList;
 }
 
 const main = async (env) => {
     let shellFile = await readFileAsync(`dev/config.json`, "utf8");
-    console.log("the contents are:" , shellFile);
+    
     shellFile = JSON.parse(shellFile);
-    let fileContents = `export let config = ${JSON.stringify(shellFile)}`;
-    console.log(shellFile);
-    writeFileSync("./config_dev/config_test_file.js", fileContents);
-    let status = await upload(`${process.cwd()}/config_dev/config_test_file.js`, `config_test_file2.js`)
+    if(!existsSync("./config"))
+    mkdirSync(`${process.cwd()}/config`)
+    let applications = await getApplicationsList(env);
+    shellFile.applications = applications;
+    let fileContents = `export let config = ${JSON.stringify(shellFile, null , 2)}`;
+    writeFileSync("./config/config.js", fileContents);
+
+    let status = await upload(`${process.cwd()}/config/config.js`, `config.js`)
         
-    console.log(typeof status);
+    
 }
 
 let environment = process.env.environment ? process.env.environment : "dev";
